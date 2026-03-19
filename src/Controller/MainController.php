1665738758cc332ec60b79a3097547b609553a21
charliefischerr <152898480+charliefischerr@users.noreply.github.com>
2026-03-19 15:26:21 -0700

    lesson 5 complete

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
-    #[Route('/')]
-    public function homepage(): Response
-    {
-        return new Response('Hello World');
-    }
+  #[Route('/')]
+  public function homepage(): Response
+  {
+    $starshipCount = 457;
+    $selectedShip = [
+      'name' => 'Twigcelerator',
+      'class' => 'Rally',
+      'captain' => 'Hans Moleman',
+      'status' => 'Owned',
+    ];
+    return $this->render('main/homepage.html.twig', [
+      'numberOfStarships' => $starshipCount,
+      'selectedShip' => $selectedShip,
+    ]);
+  }
 }
